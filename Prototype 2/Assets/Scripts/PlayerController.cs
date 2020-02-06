851c441e722e90e5e8e1c3ef283118872c92e632
Keiry577 <kr73072@srcs.org>
2020-02-06 15:32:37 -0800

    Challenge work

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -17,19 +17,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-           if(Input.GetKeyDown(KeyCode.Space))
-            {
-                //Launch a projectile from the player 
-                Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-            }
-           if (transform.position.x < -xRange)
-            {
-                transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-            }
-            if (transform.position.x > xRange)
-            {
-                transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-            }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile from the player 
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
